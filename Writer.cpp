f72d56b95d79a748e22cd520f19fb6d6dce9f9b3
Steven Li <steven.zelin.li@gmail.com>
2017-11-24 00:29:28 -0500

    Finished writing most of Writer.cpp for Task 1

--- Writer.cpp
+++ Writer.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <thread>
+#include <unistd.h>
+
+using namespace std;
 
 int main(void)
 {
-	std::cout << "I am a Writer" << std::endl;
+	// variable declaration
+	char charChoice;
+	cout << "I am a Writer" << endl;
+
+	// looping the choice
+	while (true)
+	{
+		charChoice = 'C';
+		// invalid input
+		while(toupper(charChoice) != 'Y' && toupper(charChoice) != 'N'){
+			cout << "Would you like to create a writer thread? ";
+			cin >> charChoice;
+			// if user decides to spawn writer thread 
+			if(toupper(charChoice) == 'Y'){
+				// code to run writing thread
+				// first get delay
+				int intDelay = 1;
+				cout << "What is the Delay time (sec) for the new thread? ";
+				cin >> intDelay;
+				unsigned int delay = intDelay * 1000000;
+				thread writingThread();
+			}
+			// if not then exit
+			else if(toupper(charChoice) == 'N'){
+				string temp;
+				cout << "Now exiting ...\n";
+				return 0;
+			}
+		}
+	}
 }
