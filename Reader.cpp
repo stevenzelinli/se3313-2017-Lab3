667e6c35a3c2647015534f174fab5a6746812fcb
Steven Li <steven.zelin.li@gmail.com>
2017-11-24 18:42:36 -0500

    Finished Task 1, creating branches

--- Reader.cpp
+++ Reader.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <unistd.h>
 
 int main(void)
 {
 	std::cout << "I am a reader" << std::endl;
+	// creating read stream
+	std::ifstream readStream;
+	readStream.open("writing.txt");
+	while(true){
+		if(readStream.eof()){
+			sleep(5);
+		}
+		else{
+			std::string strMessage;
+			std::getline(readStream, strMessage);
+			std::cout << strMessage<< std::endl;
+		}
+	}
 }
